f44a3b1be5413a9db509f8e3622a0c92d8c58aff
David Schramm <david.schramm@live.com>
2023-02-03 17:11:00 +0100

    add spacing

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	x86_64-w64-mingw32-gcc -o who who.c -lwtsapi32
+	x86_64-w64-mingw32-gcc -Wall -pedantic -o who who.c -lwtsapi32
 
 clean:
 	rm who.exe
--- who.c
+++ who.c
@@ -26,83 +26,83 @@ void _print_last_error_message(void) {
     LocalFree(emsg);
 }
 
-void print_client_addr(WTS_CLIENT_ADDRESS *addr){
-	switch(addr->AddressFamily){
-		case AF_INET:
-			for(int i=0;i<4;++i){
-				printf("%hhu", addr->Address[2+i]);
-				if(i<3) printf(".");
-			}
-			break;
-		case AF_INET6:
-			for(int i=0;i<10;++i){
-				printf("%hhX%hhX", addr->Address[(i<<1)], addr->Address[(i<<1)|1]);
-				if(i<9) printf(":");
-			}
-			break;
-		case AF_IPX:
-		case AF_NETBIOS:
-		case AF_UNSPEC:
-		default:
-			printf("unknown");
-	}
+void print_client_addr(WTS_CLIENT_ADDRESS *addr) {
+    switch(addr->AddressFamily) {
+    case AF_INET:
+        for(int i=0; i<4; ++i) {
+            printf("%hhu", addr->Address[2+i]);
+            if(i<3) printf(".");
+        }
+        break;
+    case AF_INET6:
+        for(int i=0; i<10; ++i) {
+            printf("%hhX%hhX", addr->Address[(i<<1)], addr->Address[(i<<1)|1]);
+            if(i<9) printf(":");
+        }
+        break;
+    case AF_IPX:
+    case AF_NETBIOS:
+    case AF_UNSPEC:
+    default:
+        printf("unknown");
+    }
 }
 
-void print_users(HANDLE server_h){
-	unsigned long num_sessions=0;
-	WTS_SESSION_INFOW *sessions;
+void print_users(HANDLE server_h) {
+    unsigned long num_sessions=0;
+    WTS_SESSION_INFOW *sessions;
 
-	if(!WTSEnumerateSessionsW(server_h, 0, 1, &sessions, &num_sessions)){
-		BLAME("Failed at EnumerateSessionsW\n");
-		return;
-	}
+    if(!WTSEnumerateSessionsW(server_h, 0, 1, &sessions, &num_sessions)) {
+        BLAME("Failed at EnumerateSessionsW\n");
+        return;
+    }
 
-	unsigned long buf_size;
-	wchar_t *username;
-	WTS_CLIENT_ADDRESS *addr;
+    unsigned long buf_size;
+    wchar_t *username;
+    WTS_CLIENT_ADDRESS *addr;
 
-	for(unsigned long i=0;i<num_sessions;++i){
-		if(!WTSQuerySessionInformationW(server_h, sessions[i].SessionId, WTSUserName, &username, &buf_size)){
-			BLAME("Error at WTSQuerySessionInformation(...,WTSUserName,...)\n");
-			continue;
-		}
-	
-		if(wcslen(username)==0){
-			if(username) WTSFreeMemory(username);
-			username=NULL;
-			continue;
-		}
-		
-		fwprintf(stdout, L"%.*ls\t", buf_size, username);
+    for(unsigned long i=0; i<num_sessions; ++i) {
+        if(!WTSQuerySessionInformationW(server_h, sessions[i].SessionId, WTSUserName, &username, &buf_size)) {
+            BLAME("Error at WTSQuerySessionInformation(...,WTSUserName,...)\n");
+            continue;
+        }
 
-		WTSFreeMemory(username);
+        if(wcslen(username)==0) {
+            if(username) WTSFreeMemory(username);
+            username=NULL;
+            continue;
+        }
 
-		if(!WTSQuerySessionInformationW(server_h, sessions[i].SessionId, WTSClientAddress, (LPWSTR *) &addr, &buf_size))
-			BLAME("Error at WTSQuerySessionInformation(...,WTSClientAddress,...)\n");
-		
-		if(buf_size)
-			print_client_addr(addr);	
-		
-		printf("\n");
+        fwprintf(stdout, L"%-48.*ls ", buf_size, username);
 
-		if(addr){
-			WTSFreeMemory(addr);
-			addr=NULL;
-		}
-	}
+        WTSFreeMemory(username);
 
-	WTSFreeMemory(sessions);
+        if(!WTSQuerySessionInformationW(server_h, sessions[i].SessionId, WTSClientAddress, (LPWSTR *) &addr, &buf_size))
+            BLAME("Error at WTSQuerySessionInformation(...,WTSClientAddress,...)\n");
+
+        if(buf_size)
+            print_client_addr(addr);
+
+        printf("\n");
+
+        if(addr) {
+            WTSFreeMemory(addr);
+            addr=NULL;
+        }
+    }
+
+    WTSFreeMemory(sessions);
 }
 
-int main(int ac, char *as[]){
-	HANDLE server_h=WTS_CURRENT_SERVER_HANDLE;
+int main(int ac, char *as[]) {
+    HANDLE server_h=WTS_CURRENT_SERVER_HANDLE;
 
-	if(ac>1)
-		server_h=WTSOpenServer(as[1]);
+    if(ac>1)
+        server_h=WTSOpenServer(as[1]);
 
-	print_users(server_h);
-	
-	WTSCloseServer(server_h);
+    print_users(server_h);
 
-	return 0;
+    WTSCloseServer(server_h);
+
+    return 0;
 }
